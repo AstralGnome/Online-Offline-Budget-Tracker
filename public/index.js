e49dbb96ca27b8a02a4ff0db561f92a811b233b9
Astral Gnome <reedkeenan@gmail.com>
2021-03-07 16:28:28 -0800

    Got rid of parseFloat in index.db and changed to parseInt

--- public/index.js
+++ public/index.js
@@ -18,8 +18,8 @@ function populateTotal() {
   // reduce transaction amounts to a single total value
   let total = transactions
     .reduce((total, t) => {
-      return total + parseFloat(t.value);
-  }, 0).toFixed(2);
+      return total + parseInt(t.value);
+  }, 0);
 
   let totalEl = document.querySelector("#total");
   totalEl.textContent = total;
